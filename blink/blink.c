d35b6acbc5a6fc04ac9ddffc45ec63eac57ef486
Jorge Eliecer Rojas Quiñones <82410234+roquijo@users.noreply.github.com>
2023-08-20 12:42:17 -0500

    Update blink.c

--- blink/blink.c
+++ blink/blink.c
@@ -1,23 +1,11 @@
-/**
- * Copyright (c) 2020 Raspberry Pi (Trading) Ltd.
- *
- * SPDX-License-Identifier: BSD-3-Clause
- */
-
-#include "pico/stdlib.h"
+#include <Python.h>
+#include <stdio.h>
 
 int main() {
-#ifndef PICO_DEFAULT_LED_PIN
-#warning blink example requires a board with a regular LED
-#else
-    const uint LED_PIN = PICO_DEFAULT_LED_PIN;
-    gpio_init(LED_PIN);
-    gpio_set_dir(LED_PIN, GPIO_OUT);
-    while (true) {
-        gpio_put(LED_PIN, 1);
-        sleep_ms(250);
-        gpio_put(LED_PIN, 0);
-        sleep_ms(250);
-    }
-#endif
+    Py_Initialize();
+    PyObject* my_module = PyImport_ImportModule("blink");    
+    PyObject* my_result = PyObject_CallObject(my_function, NULL);
+    Py_Finalize();
+
+    return 0;
 }
