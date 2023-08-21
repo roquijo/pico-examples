37c51bab8d8173795a45f1607d1fed48667cf3f0
Jorge Eliecer Rojas Quiñones <82410234+roquijo@users.noreply.github.com>
2023-08-21 18:39:56 -0500

    Update blink.c

--- blink/blink.c
+++ blink/blink.c
@@ -1,9 +1,15 @@
-#include <Python.h>
+#include <python.h>
 #include <stdio.h>
 
 int main() {
     Py_Initialize();
+
+    PyRun_SimpleString("import sys");
+    PyRun_SimpleString("import os");
+    PyRun_SimpleString("sys.path.append(os.getcwd())");
+
     PyObject* my_module = PyImport_ImportModule("blink");    
+    PyObject* my_function = PyObject_GetAttrString(my_module, "print")
     PyObject* my_result = PyObject_CallObject(my_function, NULL);
     Py_Finalize();
 
