3d6c7400b720aea950ab5cc0d0f1fc5526f983d6
Jorge Eliecer Rojas Quiñones <82410234+roquijo@users.noreply.github.com>
2023-08-22 17:54:08 -0500

    Update blink.py

--- blink/blink.py
+++ blink/blink.py
@@ -1,9 +1,12 @@
-from machine import Pin, Timer
-
-led = Pin("LED", Pin.OUT)
-tim = Timer()
-def tick(timer):
-    global led
-    led.toggle()
-
-tim.init(freq=2.5, mode=Timer.PERIODIC, callback=tick)
+# Paso 1
+from machine import Pin
+# Paso 2
+import time
+# Paso 3
+led_1 = Pin(5, Pin.OUT)
+while True:
+    # Paso 4
+    led_1.value(1)
+    time.sleep(0.5)
+    led_1.value(0)
+    time.sleep(0.5)
