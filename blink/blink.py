c414e30c28a80f9c0c03c54ff6f6b0d6c4aff942
Jorge Eliecer Rojas Quiñones <82410234+roquijo@users.noreply.github.com>
2023-08-27 16:33:35 -0500

    Update blink.py

--- blink/blink.py
+++ blink/blink.py
@@ -1,12 +1,8 @@
-# Paso 1
 from machine import Pin
-# Paso 2
-import time
-# Paso 3
-led_1 = Pin(5, Pin.OUT)
+from time import sleep
+
+led = Pin(5, Pin.OUT)
+
 while True:
-    # Paso 4
-    led_1.value(1)
-    time.sleep(0.5)
-    led_1.value(0)
-    time.sleep(0.5)
+    led.toogle()
+    sleep(0.5)
